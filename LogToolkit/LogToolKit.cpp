132cfcfe2900dcd425ab5fbbef53c321483d6c8b
Haoran Li <42954337+lihaoranIcefire@users.noreply.github.com>
2024-11-22 09:13:58 -0500

    ok

--- LogToolkit/LogToolKit.cpp
+++ LogToolkit/LogToolKit.cpp
@@ -5,33 +5,35 @@
 
 using namespace std;
 
+using Int = int;
+
 class ISymb {
 public:
-    std::tuple<int> args;
-    std::tuple<int> i;
-    std::tuple<int> n;
+    vector<Int> args;
+    vector<Int> i;
+    vector<Int> n;
     int m;
     int weight;
 
-    ISymb(const std::tuple<int>& args) : args(args), weight(0) {
+    ISymb(const vector<Int>& args) : args(args), weight(-1) {
         // Validate the number of arguments
         if (args.size() % 2 == 0 || args.size() < 3) {
             throw std::invalid_argument("The number of arguments should be odd and at least 3");
         }
 
         m = args.size() / 2 - 1;
-        for (int r = 0; r <= m; ++r) {
-            int i_r = (r > 0) ? args[2 * r] : 0;
-            int n_r = args[1 + 2 * r];
-            if (n_r < 1 || (r > 0 && i_r >= args[2 * r + 1])) {
+        i.push_back(args[0]);
+        for (Int r = 0; r <= m; r++) {
+            n.push_back(args[1 + 2 * r]);
+            i.push_back(args[2 + 2 * r]);
+            if (n[r] < 1 || i[r] < i[r+1]) {
                 throw std::invalid_argument("Invalid arguments");
             }
-            weight += n_r;
+            weight += n[r];
         }
     }
 
     bool operator < (const ISymb& other) const {
-        return std::tie(weight, args) < std::tie(other.weight, other.args);
     }
 
     // For debugging
@@ -60,28 +62,7 @@ namespace std {
 }
 
 int main() {
-    try {
-        // Create instances of ISymb
-        ISymb a({1, 2, 3});
-        ISymb b({1, 2, 3, 4, 5});
-        ISymb c({1, 3, 5, 7, 9});
-
-        // Create a map with ISymb as the key
-        std::unordered_map<ISymb, int> symbMap;
-
-        // Insert elements
-        symbMap[a] = 10;
-        symbMap[b] = 20;
-        symbMap[c] = 30;
-
-        // Iterate and print
-        for (const auto& [key, value] : symbMap) {
-            key.print();
-            std::cout << " -> " << value << std::endl;
-        }
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
+    
 
     return 0;
 }
