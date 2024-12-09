0cf62211a55edaf385b5bbb12b8199ad3b554ad1
Haoran Li <42954337+lihaoranIcefire@users.noreply.github.com>
2024-12-09 09:42:37 -0500

    ok

--- LogToolkit/LogToolKit.jl
+++ LogToolkit/LogToolKit.jl
@@ -2,7 +2,7 @@ module LogToolKit
 
 
 
-export Symb, ISymb, HSymb, monomial, polynomial, tensor, tensors, Phi, Phi_inv, fundamental_column
+export Symb, ISymb, HSymb, monomial, lincomb, polynomial, tensor, Phi, Phi_inv, fundamental_column
 
 
 
@@ -12,7 +12,163 @@ using Combinatorics
 
 
 
+function ordered_partitions(n::Int, k::Int; positive=true)
+    if positive
+        return union((unique(permutations(unordered_partition)) for unordered_partition in partitions(n + k, k))...)
+    else
+        return [partition .- 1 for partition in ordered_partitions(n, k)]
+    end
+end
+
+
+
 abstract type Symb end
+
+
+
+struct monomial{T}
+    # T must be comparable type that has the latex_repr attribute
+    vars::Tuple{Vararg{Union{Pair{T, Int}}}}
+    deg::Int
+    latex_repr::String
+
+    # constructor for monomial{T}, Int refers to the exponent
+    function monomial{T}(vars::Vararg{Union{Tuple{T, Int}, Pair{T, Int}, T}}) where T
+        if length(vars) < 1
+            throw(ArgumentError("vars cannot be empty"))
+        end
+        # multiply the variables
+        counter = Dict{T, Int}()
+        for var in vars
+            (key, value) = typeof(var) <: T ? (var, 1) : var
+            if value <= 0
+                throw(ArgumentError("the exponent must be at least 1"))
+            end
+            counter[key] = get(counter, key, 0) + value
+        end
+
+        # sort the variables
+        vars = Tuple(sort(collect(counter)))
+
+        # compute the degree of the monomial
+        deg = sum([exponent for (_, exponent) in vars])
+
+        # generate latex_repr
+        latex_repr = ""
+        for (var, exponent) in vars
+            if exponent == 1
+                latex_repr *= "$(var.latex_repr)"
+            else
+                latex_repr *= "$(var.latex_repr)^{$(exponent)}"
+            end
+        end
+
+        return new{T}(vars, deg, latex_repr)
+    end
+end
+
+struct tensor{T}
+    # T must be a type that has the latex_repr attribute
+    args::Tuple{Vararg{T}}
+    latex_repr::String
+
+    # constructor for tensor{T}
+    function tensor{T}(args::Vararg{T}) where T
+        if length(args) < 2
+            throw(ArgumentError("the number of vars must be at least 2"))
+        end
+        return new{T}(args, join(map(x -> x.latex_repr, args), "\\otimes "))
+    end
+end
+
+struct lincomb{T}
+    # T must be comparable type that has the latex_repr attribute
+    terms::Tuple{Vararg{Union{T, Pair{T, Number}}}}
+    intercept::Number
+    latex_repr::String
+
+    # constructor for lincomb, Number refers to the coefficient
+    # Number is in the second argument to ease the sorting of variables
+    function lincomb{T}(terms::Vararg{Union{T, Tuple{T, Number}, Pair{T, Int}}}; intercept=0) where T
+        if length(terms) < 1
+            throw(ArgumentError("the linear combination cannot be empty"))
+        end
+        # add the variables
+        counter = Dict{T, Number}()
+        for term in terms
+            (key, value) = typeof(term) <: T ? (term, 1) : term
+            counter[key] = get(counter, key, 0) + value
+        end
+
+        # sort the variables
+        terms = Tuple(sort(collect(counter)))
+
+        # generate latex_repr
+        argstr = String[]
+        if intercept != 0
+            push!(argstr, "($intercept)")
+        end
+        for term in terms
+            if term.second == 0
+                continue
+            elseif term.second == 1
+                push!(argstr, term.first.latex_repr)
+            else
+                push!(argstr, "($(term.second))" * term.first.latex_repr)
+            end
+        end
+        latex_repr = join(argstr, '+')
+
+        return new{T}(terms, intercept, latex_repr)
+    end
+end
+polynomial{T} = lincomb{Union{T, monomial{T}}}
+
+## Overloading the Base operators ==, <, *, +
+
+Base.isequal(a::Number, b::Union{Symb, monomial{Symb}, tensor{Symb}}) = false
+Base.isequal(a::Symb, b::monomial{Symb}) = b.deg == 1 && a == b.vars[1].first
+Base.isequal(a::Number, b::polynomial{Symb}) = length(b.terms) == 0 && a == b.intercept
+Base.isequal(a::Union{Symb, monomial{Symb}}, b::polynomial{Symb}) = b.intercept == 0 && length(b.terms) == 1 && a == b.terms[1].first && b.terms[1].second == 1
+
+
+Base.isless(a::Number, b::Union{Symb, monomial{Symb}, tensor{Symb}}) = true
+Base.isless(a::T, b::monomial{T}) where T = b.deg > 1 ? true : a < b.vars[1].first
+Base.isless(a::monomial{T}, b::T) where T = b < a
+Base.isless(m1::monomial{T}, m2::monomial{T}) where T = m1.deg != m2.deg ? m1.deg < m2.deg : m1.vars < m2.vars
+Base.isless(t1::tensor{T}, t2::tensor{T}) where T = length(t1) != length(t2) ? length(t1) < length(t2) : t1.terms < t2.terms
+
+
+Base.:+(a::Number, b::Union{Symb, monomial{Symb}}) = a == 0 ? b : polynomial{Symb}(b; intercept=a)
+Base.:+(a::Union{Symb, monomial{Symb}}, b::Number) = b + a
+Base.:+(a::Number, b::polynomial{Symb}) = polynomial{Symb}(b.terms; intercept=a+b.intercept)
+Base.:+(a::polynomial{Symb}, b::Number) = b + a
+Base.:+(a::Union{Symb, monomial{Symb}}, b::Union{Symb, monomial{Symb}}) = polynomial{Symb}(a, b)
+Base.:+(a::Union{Symb, monomial{Symb}}, b::polynomial{Symb}) = polynomial{Symb}(a, b.terms...; intercept=b.intercept)
+Base.:+(a::polynomial{Symb}, b::Union{Symb, monomial{Symb}}) = b + a
+Base.:+(a::polynomial{Symb}, b::polynomial{Symb}) = polynomial{Symb}(a.terms, b.terms...; intercept=a.intercept+b.intercept)
+
+
+Base.:*(a::Number, b::Union{Symb, monomial{Symb}}) = a == 0 ? 0 : polynomial{Symb}((b, a))
+Base.:*(a::Union{Symb, monomial{Symb}}, b::Number) = b * a
+Base.:*(a::Number, b::polynomial{Symb}) = a == 0 ? 0 : polynomial{Symb}(map(x->(x.first, a*x.second), b.terms)...; intercept=a*b.intercept)
+Base.:*(a::polynomial{Symb}, b::Number) = b * a
+Base.:*(a::Symb, b::Symb) = monomial{Symb}(a, b)
+Base.:*(a::Symb, b::monomial{Symb}) = monomial{Symb}(a, b.vars...)
+Base.:*(a::monomial{Symb}, b::Symb) = b * a
+Base.:*(a::monomial{Symb}, b::monomial{Symb}) = monomial{Symb}(a.vars..., b.vars...)
+Base.:*(a::Union{Symb, monomial{Symb}}, b::polynomial{Symb}) = polynomial{Symb}((a, b.intercept), map(x->(a*x.first, x.second), b.terms)...)
+Base.:*(a::polynomial{Symb}, b::Union{Symb, monomial{Symb}}) = b * a
+Base.:*(a::polynomial{Symb}, b::polynomial{Symb}) = polynomial{Symb}(
+    vec([(t1.first * t2.first, t1.second * t2.second) for t1 in a.terms, t2 in b.terms])...,
+    [(t.first, a.intercept * t.second) for t in b.terms]...,
+    [(t.first, b.intercept * t.second) for t in a.terms]...
+    ; intercept = a.intercept * b.intercept
+)
+
+
+
+
 
 
 
@@ -45,7 +201,7 @@ struct ISymb <: Symb
         if m < 0 || any(n(r) < 1 || i(r) >= i(r + 1) for r in 1:m)
             throw(ArgumentError("The arguments are not valid"))
         end
-        
+
         # Form the latex string
         for r in 0:m
             push!(argstrs, "a_{$(i(r))}")
@@ -76,16 +232,6 @@ Base.isless(a::ISymb, b::ISymb) = begin
     end
 
     return a.i(0) < b.i(0)
-end
-
-Base.:*(a::Symb, b::Symb) = begin
-    if a == b
-        return monomial((a, 2))
-    elseif a < b
-        return monomial((a,1), (b,1))
-    else
-        return monomial((b,1), (a,1))
-    end
 end
 
 function Phi(I::ISymb, d::Int)
@@ -195,115 +341,42 @@ function differential(H::HSymb)
 end
 
 function Phi_inv(H::HSymb)
-    return ISymb(0, 1, vcat(([H.i[r], H.n(r)] for r in 1:H.d)...)..., H.i[end])
+    return (-1)^H.d * ISymb(0, 1, vcat(([H.i[r], H.n(r)] for r in 1:H.d)...)..., H.i[end])
 end
 
 
 
-struct monomial
-    vars::Tuple{Vararg{Tuple{Any, Int}}}
-    deg::Int
-    coef::Number
-    latex_repr::String
+function fundamental_column(h::HSymb)
+    visited = Set{HSymb}()
+    result = []
 
-    function monomial(vars::Vararg{Tuple{Any, Int}}; coef::Number = 1)
-        # Validate the arguments
-        if any(exponent < 1 for (var, exponent) in vars)
-            throw(ArgumentError("The exponents should be at least 1"))
+    function dfs(H::HSymb)
+        if H in visited
+            return
         end
 
-        vars = Tuple(sort(collect(vars)))
-        deg = length(vars)>0 ? sum(exponent for (_, exponent) in vars) : 0
+        push!(result, Phi_inv(H))
+        push!(visited, H)
 
-        latex_repr = (coef == 1 ? "" : "$(coef)")
-        for (var, exponent) in vars
-            if exponent == 1
-                latex_repr *= "$(var.latex_repr)"
-            elseif exponent > 1
-                latex_repr *= "$(var.latex_repr)^{$(exponent)}"
+        if H.d == 1 && H.n(1) == 1
+            return
+        end
+
+        for r in 1:H.d
+            if H.n(r) > 1
+                dfs(HSymb(H.args[1:2*r-1]..., H.n(r)-1, H.args[2*r+1:end]...))
+            elseif r == H.d
+                dfs(HSymb(H.args[1:end-3]..., H.m(H.d)+H.m(H.d+1)))
+            else
+                dfs(HSymb(H.args[1:2*r-2]..., H.m(r)+H.m(r+1), H.args[2*r+2:end]...))
+                dfs(HSymb(H.args[1:2*r-1]..., H.n(r+1), H.m(r+1)+H.m(r+2), H.args[2*r+4:end]...))
             end
         end
-
-        return new(vars, deg, coef, latex_repr)
-    end
-end
-
-Base.isless(m1::monomial, m2::monomial) = begin
-    if m1.deg != m2.deg
-        return m1.deg < m2.deg
     end
 
-    for ((var1, exp1), (var2, exp2)) in zip(m1.vars, m2.vars)
-        if var1 != var2
-            return var1 < var2
-        elseif exp1 != exp2
-            return exp1 < exp2
-        end
-    end
-    
-    return false
+    dfs(h)
+    return [1, sort(result)...]
 end
-
-
-struct polynomial
-    terms::Tuple{Vararg{monomial}}
-    deg::Int
-    latex_repr::String
-
-    function polynomial(terms::Vararg{monomial})
-        terms = Tuple(sort(collect(terms)))
-        return new(terms, maximum([term.deg for term in terms]), join(map(x->x.latex_repr, terms), '+'))
-    end
-end
-
-
-
-struct tensor
-    args::Tuple{Vararg{Any}}
-    weight::Int
-    coef::Number
-    latex_repr::String
-
-    function tensor(args::Vararg{Any}; coef::Number = 1)
-        args = Tuple(sort(collect(args)))
-        latex_repr = (coef == 1 ? "" : "$(coef)") * join(map(x->x.latex_repr, args), "\\otimes ")
-
-        return new(args, length(args), coef, latex_repr)
-    end
-end
-
-Base.isless(t1::tensor, t2::tensor) = begin
-    if t1.weight != t2.weight
-        return t1.weight < t2.weight
-    else
-        return t1.terms < t2.terms
-    end
-    return false
-end
-
-
-
-struct tensors
-    terms::Tuple{Vararg{tensor}}
-    weight::Int
-    latex_repr::String
-
-    function tensors(terms::Vararg{tensor})
-        terms = Tuple(sort(collect(terms)))
-        return new(terms, maximum([term.weight for term in terms]), join(map(x->x.latex_repr, terms), '+'))
-    end
-end
-
-
-
-function nonnegative_partitions(n::Int, k::Int)
-    positive_partitions = union((unique(permutations(unordered_partition)) for unordered_partition in partitions(n + k, k))...)
-    return [partition .- 1 for partition in positive_partitions]
-end
-
-
-
-
 
 
 
